f9739a49d0207dfdbfcfbfaf31eea01b61b99298
BONiii1212 <641564581@qq.com>
2022-06-05 18:21:05 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-博客的后台：Webpack+React
+## 博客后台开发
+
+SPA单页应用
+
+### 技术栈
+
+React、React Router、marked
+
+### 实现功能
+
+文章添加、删除和编辑功能
--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Main from "./Pages/Main";
 
 function App() {
   const {user} = useAuth()
+  console.log(user)
   return (
     <div className="App">
       {user?.token?<Main/>:<Login/>}
